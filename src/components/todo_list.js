dda007935a161d4356eb8a65c2c9efb8a0391f36
Scott Bowler <scott.bowler@learningfuze.com>
2018-08-09 14:26:44 -0700

    Completed displaying list data

--- src/components/todo_list.js
+++ src/components/todo_list.js
@@ -3,18 +3,14 @@ import listData from '../data/todo';
 
 class TodoList extends Component {
     render(){
-        console.log('List Data:', listData);
-        const listElements = listData.map( (item, index) => {
-            return <li key={item._id}>{item.title}</li>;
+        const listElements = listData.map( item => {
+            return <li className="collection-item" key={item._id}>{item.title}</li>;
         });
 
         return (
-            <div>
-                <h1>This will be the list</h1>
-                <ul>
-                    {listElements}
-                </ul>
-            </div>
+            <ul className="collection">
+                {listElements}
+            </ul>
         );
     }
 }
