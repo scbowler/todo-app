8aaead3f1037ad73e98144257f92b9def948641f
Scott Bowler <scott.bowler@learningfuze.com>
2018-08-09 17:06:06 -0700

    Axios working

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 import AddItem from './add_item';
 import TodoList from './todo_list';
 import listData from '../data/todo';
+import axios from 'axios';
+
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=c618_demouser';
 
 class App extends Component {
     constructor(props){
@@ -17,24 +21,41 @@ class App extends Component {
         this.getListData();
     }
 
-    addItem(item){
-        item._id = new Date().getTime();
+    async addItem(item){
+        try {
+            await axios.post(`${BASE_URL}/todos${API_KEY}`, item);
+
+            this.getListData();
+        } catch(err){
+            console.log('Something went wrong!:', err.message);
+        }
+        
+    }
+
+    async getListData(){
+        const resp = await axios.get(`${BASE_URL}/todos${API_KEY}`);
 
         this.setState({
-            items: [item, ...this.state.items]
+            items: resp.data.todos
         });
     }
 
-    getListData(){
-        // This is where you would call the server for your data
+    // getListData(){
+    //     // This is where you would call the server for your data
 
-        this.setState({
-            items: listData
-        });
-    }
+    //     // http://api.reactprototypes.com/todos?key=c418_demouser
+    //     axios.get(`${BASE_URL}/todos${API_KEY}`).then((resp) => {
+            
+    //         this.setState({
+    //             items: resp.data.todos
+    //         });
+
+    //     }).catch((err) => {
+    //         console.log('There was an error!:', err.message);
+    //     });
+    // }
 
     render(){
-        console.log('App State:', this.state);
         return (
             <div className="container">
                 <h1 className="center">To Do List</h1>
